df178ff90bf7ab94a269808c3198f9e520928afd
Heber Marques <heber.f.marques@gmail.com>
2025-08-20 18:12:56 -0300

    added cookie configuration

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,8 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "cookie";
+})
+.AddCookie("cookie", options =>
+{
+    options.Cookie.Name = "__Host-AuthCookie";
+    options.Cookie.SameSite = SameSiteMode.Strict;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.Path = "/";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(1);
+    options.SlidingExpiration = true;
+    options.LoginPath = "bff/SignInUser";
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -11,8 +28,17 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 var summaries = new[]
 {
