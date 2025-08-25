13ad0efb8ecba33d8e4d89df8654c3ed1eb6d5d0
Héber Marques <heber.f.marques@gmail.com>
2025-08-25 18:41:04 -0300

    added bffapi

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,67 +1,45 @@
+using WebApi.Apis;
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddOpenApi();
 
 builder.Services.AddHttpClient();
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = "cookie";
-})
-.AddCookie("cookie", options =>
-{
-    options.Cookie.Name = "__Host-AuthCookie";
-    options.Cookie.SameSite = SameSiteMode.Strict;
-    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-    options.Cookie.Path = "/";
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(1);
-    options.SlidingExpiration = true;
-    options.LoginPath = "bff/SignInUser";
-});
+builder.Services.AddAuthentication(options => { options.DefaultScheme = "cookie"; })
+    .AddCookie("cookie", options =>
+    {
+        options.Cookie.Name = "__Host-AuthCookie";
+        options.Cookie.SameSite = SameSiteMode.Strict;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.Path = "/";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(1);
+        options.SlidingExpiration = true;
+        options.LoginPath = "/bff/SignInUser";
+    });
 
 builder.Services.AddAuthorization();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-var summaries = new[]
+app.UseSwagger();
+app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    app.Map("/", () => Results.Redirect("/swagger"));
 }
+
+app.AddApisFromBff();
+
+await app.RunAsync();
